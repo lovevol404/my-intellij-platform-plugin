89681594b6c017a18e45f54715567623715b20ea
GitHub Action <action@github.com>
2020-08-20 06:37:23 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/lovevol404/myintellijplatformplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.lovevol404.myintellijplatformplugin.services
+
+import com.github.lovevol404.myintellijplatformplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
